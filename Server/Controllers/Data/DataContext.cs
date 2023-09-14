21e53bbc22bc6a0402bebe9683285ba9eebf4a7e
Gabriel <camed.tf@gmail.com>
2023-09-14 19:32:53 +0200

    Removed default Blazor WebAssembly stuff, added SQLite, Entity, First Migration added

---
+++ Server/Controllers/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using camedx_blog.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace camedx_blog.Server.Controllers.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+    }
+}
---
+++ Server/Migrations/20230914172941_Init.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace camedx_blog.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogPosts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    ShortDescription = table.Column<string>(type: "TEXT", nullable: false),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsPublished = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsModified = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPosts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogPosts");
+        }
+    }
+}
