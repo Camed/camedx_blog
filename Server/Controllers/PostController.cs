09a8b6aee3330610b8d5ed82e3ed8b20385b636a
Gabriel <camed.tf@gmail.com>
2023-09-14 18:49:00 +0200

    fixed spelling

--- Server/Controllers/PostController.cs
+++ Server/Controllers/PostController.cs
@@ -13,16 +13,16 @@ namespace camedx_blog.Server.Controllers
             new BlogPost()
             {
                 Id = new Guid("062D44BD-14FF-4064-A21E-9B9CE7F26DC4"),
-                Title = "Papież xD",
-                Content = "Co Jan Paweł II tak naprawdę robił z dziećmi xD?",
-                ShortDescription = "Krótka historia o Janie Pawle II"
+                Title = "test123",
+                Content = "12345",
+                ShortDescription = "p0p3k"
             },
             new BlogPost()
             {
                 Id = new Guid("16F7580F-6FF2-4D98-98AB-4C6E6113D2C2"),
-                Title = "Krzysztof Gonciarz",
-                Content = "Kasia nie wróci bo znalazła Chada z Brazylii...",
-                ShortDescription = "Wiemy już, dlaczego Kasia nie wróci!",
+                Title = "krzysztof",
+                Content = "brazylia",
+                ShortDescription = "test",
             },
         };
 
